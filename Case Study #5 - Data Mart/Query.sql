49625fda80e67cb129cff82fec1c3df17f623204
Anh Nguyen <nguyenmaianh.96@gmail.com>
2022-09-04 13:08:02 +0700

    update case study 5

---
+++ Case Study #5 - Data Mart/Query.sql
@@ -0,0 +1,282 @@
+/* --------------------
+   Case Study Questions
+   --------------------*/
+
+-- 1. Data Cleansing Steps --
+-- In a single query, perform the following operations and generate a new table in the data_mart schema named clean_weekly_sales:
+	-- Convert the week_date to a DATE format
+	-- Add a week_number as the second column for each week_date value, for example any value from the 1st of January to 7th of January will be 1, 8th to 14th will be 2 etc
+	-- Add a month_number with the calendar month for each week_date value as the 3rd column
+	-- Add a calendar_year column as the 4th column containing either 2018, 2019 or 2020 values
+	-- Add a new column called age_band after the original segment column using the following mapping on the number inside the segment value
+		-- segment	age_band
+		-- 1	Young Adults
+		-- 2	Middle Aged
+		-- 3 or 4	Retirees
+	-- Add a new demographic column using the following mapping for the first letter in the segment values:	
+		-- segment	demographic
+		-- C	Couples
+		-- F	Families
+	-- Ensure all null string values with an "unknown" string value in the original segment column as well as the new age_band and demographic columns
+	-- Generate a new avg_transaction column as the sales value divided by transactions rounded to 2 decimal places for each record
+
+DROP TABLE IF EXISTS clean_weekly_sales;
+CREATE TABLE clean_weekly_sales
+SELECT
+	STR_TO_DATE(week_date, '%d/%m/%y') AS week_date,
+    WEEK(STR_TO_DATE(week_date, '%d/%m/%y')) AS week_number,
+    MONTH(STR_TO_DATE(week_date, '%d/%m/%y')) AS month_number,
+    YEAR(STR_TO_DATE(week_date, '%d/%m/%y')) AS calendar_year,
+    region, 
+    platform, 
+    CASE
+		WHEN segment = 'null' THEN 'unknown'
+        ELSE segment
+    END AS segment,
+    CASE
+		WHEN segment LIKE '%1' THEN 'Young Adults'
+        WHEN segment LIKE '%2' THEN 'Middle Aged'
+        WHEN segment LIKE '%3' OR segment LIKE '%4' THEN 'Retirees'
+        ELSE 'unknown'
+    END AS age_band,
+    CASE
+		WHEN segment LIKE 'C%' THEN 'Couples'
+        WHEN segment LIKE 'F%' THEN 'Families'
+		ELSE 'unknown'
+    END AS demographic,
+    customer_type,
+    transactions,
+    sales,
+    ROUND(sales/transactions, 2) AS avg_transaction
+FROM weekly_sales;
+SELECT * FROM clean_weekly_sales;
+
+-- 2. Data Exploration --
+-- 1. What day of the week is used for each week_date value?
+SELECT DISTINCT DAYNAME(week_date) AS week_day
+FROM clean_weekly_sales;
+
+-- 2. What range of week numbers are missing from the dataset?
+WITH RECURSIVE week_numbers_cte(week_number) AS(
+	SELECT 1
+    UNION
+    SELECT week_number + 1
+	FROM week_numbers_cte
+    WHERE week_number + 1 <= 52)
+SELECT GROUP_CONCAT(week_number) AS week_number_missing
+FROM week_numbers_cte
+WHERE week_number NOT IN (SELECT week_number FROM clean_weekly_sales);
+
+-- 3. How many total transactions were there for each year in the dataset?
+SELECT 
+	calendar_year,
+    SUM(transactions) AS total_transactions
+FROM clean_weekly_sales
+GROUP BY calendar_year
+ORDER BY calendar_year;
+
+-- 4. What is the total sales for each region for each month?
+SELECT
+	region,
+    month_number,
+	SUM(sales) AS total_sales
+FROM clean_weekly_sales
+GROUP BY region, month_number
+ORDER BY region, month_number;
+
+-- 5. What is the total count of transactions for each platform
+SELECT
+	platform,
+    SUM(transactions) AS total_transactions
+FROM clean_weekly_sales
+GROUP BY platform;
+
+-- 6. What is the percentage of sales for Retail vs Shopify for each month?
+SELECT
+	month_number,
+    ROUND(SUM(CASE WHEN platform = 'Retail' THEN sales END)/SUM(sales) * 100, 2) AS retail_percent,
+    ROUND(SUM(CASE WHEN platform = 'Shopify' THEN sales END)/SUM(sales) * 100, 2) AS shopify_percent
+FROM clean_weekly_sales
+GROUP BY month_number
+ORDER BY month_number;
+
+-- 7. What is the percentage of sales by demographic for each year in the dataset?
+SELECT
+	calendar_year,
+    ROUND(SUM(CASE WHEN demographic = 'Couples' THEN sales END)/SUM(sales) * 100, 2) AS couples_percent,
+    ROUND(SUM(CASE WHEN demographic = 'Families' THEN sales END)/SUM(sales) * 100, 2) AS families_percent,
+    ROUND(SUM(CASE WHEN demographic = 'unknown' THEN sales END)/SUM(sales) * 100, 2) AS unknown_percent
+FROM clean_weekly_sales
+GROUP BY calendar_year
+ORDER BY calendar_year;
+
+-- 8. Which age_band and demographic values contribute the most to Retail sales?
+SELECT
+	age_band,
+    demographic,
+    SUM(sales) AS total_sales,
+    ROUND(SUM(sales)/ SUM(SUM(sales)) OVER() * 100, 2) AS sales_percent
+FROM clean_weekly_sales
+WHERE platform = 'Retail'
+GROUP BY age_band, demographic
+ORDER BY total_sales DESC
+LIMIT 2;
+
+-- 9. Can we use the avg_transaction column to find the average transaction size for each year for Retail vs Shopify? If not - how would you calculate it instead?
+SELECT
+	calendar_year,
+    platform,
+    ROUND(SUM(sales)/SUM(transactions), 2) AS avg_transaction_group
+FROM clean_weekly_sales
+GROUP BY calendar_year, platform
+ORDER BY calendar_year;
+
+-- 3. Before & After Analysis --
+-- This technique is usually used when we inspect an important event and want to inspect the impact before and after a certain point in time.
+-- Taking the week_date value of 2020-06-15 as the baseline week where the Data Mart sustainable packaging changes came into effect.
+-- We would include all week_date values for 2020-06-15 as the start of the period after the change and the previous week_date values would be before
+-- Using this analysis approach - answer the following questions:
+-- 1. What is the total sales for the 4 weeks before and after 2020-06-15? What is the growth or reduction rate in actual values and percentage of sales?
+WITH cte AS(
+	SELECT *,
+		CASE
+			WHEN week_date < '2020-06-15' AND week_date >= SUBDATE('2020-06-15', INTERVAL 4 WEEK) THEN 'before'
+			WHEN week_date >= '2020-06-15' AND week_date < ADDDATE('2020-06-15', INTERVAL 4 WEEK) THEN 'after'
+		END AS period
+	FROM clean_weekly_sales)
+SELECT
+	SUM(CASE WHEN period = 'before' THEN sales END) AS before_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) AS after_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END) AS value_diff,
+	ROUND((SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END))/SUM(CASE WHEN period = 'before' THEN sales END)*100, 2) AS percent_diff
+FROM cte;
+
+-- 2. What about the entire 12 weeks before and after?
+WITH cte AS(
+	SELECT *,
+		CASE
+			WHEN week_date < '2020-06-15' AND week_date >= SUBDATE('2020-06-15', INTERVAL 12 WEEK) THEN 'before'
+			WHEN week_date >= '2020-06-15' AND week_date < ADDDATE('2020-06-15', INTERVAL 12 WEEK) THEN 'after'
+		END AS period
+	FROM clean_weekly_sales)
+SELECT
+	SUM(CASE WHEN period = 'before' THEN sales END) AS before_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) AS after_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END) AS value_diff,
+	ROUND((SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END))/SUM(CASE WHEN period = 'before' THEN sales END)*100, 2) AS percent_diff
+FROM cte;
+
+-- 3. How do the sale metrics for these 2 periods before and after compare with the previous years in 2018 and 2019?
+WITH cte AS(
+	SELECT *,
+		CASE
+			WHEN week_number < WEEK('2020-06-15') AND week_number >= WEEK('2020-06-15') - 12 THEN 'before'
+			WHEN week_number >= WEEK('2020-06-15') AND week_number < WEEK('2020-06-15') + 12 THEN 'after'
+		END AS period
+	FROM clean_weekly_sales)
+SELECT
+	calendar_year,
+	SUM(CASE WHEN period = 'before' THEN sales END) AS before_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) AS after_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END) AS value_diff,
+	ROUND((SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END))/SUM(CASE WHEN period = 'before' THEN sales END)*100, 2) AS percent_diff
+FROM cte
+GROUP BY calendar_year
+ORDER BY calendar_year;
+
+-- 4. Bonus Question
+-- Which areas of the business have the highest negative impact in sales metrics performance in 2020 for the 12 week before and after period?
+	-- region
+WITH cte AS(
+	SELECT *,
+		CASE
+			WHEN week_number < WEEK('2020-06-15') AND week_number >= WEEK('2020-06-15') - 12 THEN 'before'
+			WHEN week_number >= WEEK('2020-06-15') AND week_number < WEEK('2020-06-15') + 12 THEN 'after'
+		END AS period
+	FROM clean_weekly_sales)
+SELECT
+	region,
+    calendar_year,
+	SUM(CASE WHEN period = 'before' THEN sales END) AS before_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) AS after_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END) AS value_diff,
+	ROUND((SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END))/SUM(CASE WHEN period = 'before' THEN sales END)*100, 2) AS percent_diff
+FROM cte
+GROUP BY region, calendar_year
+ORDER BY region;
+
+	-- platform
+WITH cte AS(
+	SELECT *,
+		CASE
+			WHEN week_number < WEEK('2020-06-15') AND week_number >= WEEK('2020-06-15') - 12 THEN 'before'
+			WHEN week_number >= WEEK('2020-06-15') AND week_number < WEEK('2020-06-15') + 12 THEN 'after'
+		END AS period
+	FROM clean_weekly_sales)
+SELECT
+	platform,
+    calendar_year,
+	SUM(CASE WHEN period = 'before' THEN sales END) AS before_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) AS after_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END) AS value_diff,
+	ROUND((SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END))/SUM(CASE WHEN period = 'before' THEN sales END)*100, 2) AS percent_diff
+FROM cte
+GROUP BY platform, calendar_year
+ORDER BY platform;
+
+	-- age_band
+WITH cte AS(
+	SELECT *,
+		CASE
+			WHEN week_number < WEEK('2020-06-15') AND week_number >= WEEK('2020-06-15') - 12 THEN 'before'
+			WHEN week_number >= WEEK('2020-06-15') AND week_number < WEEK('2020-06-15') + 12 THEN 'after'
+		END AS period
+	FROM clean_weekly_sales)
+SELECT
+	age_band,
+    calendar_year,
+	SUM(CASE WHEN period = 'before' THEN sales END) AS before_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) AS after_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END) AS value_diff,
+	ROUND((SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END))/SUM(CASE WHEN period = 'before' THEN sales END)*100, 2) AS percent_diff
+FROM cte
+GROUP BY age_band, calendar_year
+ORDER BY age_band;
+
+	-- demographic
+WITH cte AS(
+	SELECT *,
+		CASE
+			WHEN week_number < WEEK('2020-06-15') AND week_number >= WEEK('2020-06-15') - 12 THEN 'before'
+			WHEN week_number >= WEEK('2020-06-15') AND week_number < WEEK('2020-06-15') + 12 THEN 'after'
+		END AS period
+	FROM clean_weekly_sales)
+SELECT
+	demographic,
+    calendar_year,
+	SUM(CASE WHEN period = 'before' THEN sales END) AS before_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) AS after_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END) AS value_diff,
+	ROUND((SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END))/SUM(CASE WHEN period = 'before' THEN sales END)*100, 2) AS percent_diff
+FROM cte
+GROUP BY demographic, calendar_year
+ORDER BY demographic;
+    
+	-- customer_type
+WITH cte AS(
+	SELECT *,
+		CASE
+			WHEN week_number < WEEK('2020-06-15') AND week_number >= WEEK('2020-06-15') - 12 THEN 'before'
+			WHEN week_number >= WEEK('2020-06-15') AND week_number < WEEK('2020-06-15') + 12 THEN 'after'
+		END AS period
+	FROM clean_weekly_sales)
+SELECT
+	customer_type,
+    calendar_year,
+	SUM(CASE WHEN period = 'before' THEN sales END) AS before_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) AS after_sales,
+    SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END) AS value_diff,
+	ROUND((SUM(CASE WHEN period = 'after' THEN sales END) - SUM(CASE WHEN period = 'before' THEN sales END))/SUM(CASE WHEN period = 'before' THEN sales END)*100, 2) AS percent_diff
+FROM cte
+GROUP BY customer_type, calendar_year
+ORDER BY customer_type;
---
+++ Case Study #5 - Data Mart/README.md
@@ -0,0 +1,147 @@
+# Case Study #5 - Data Mart
+
+<img src='https://8weeksqlchallenge.com/images/case-study-designs/5.png' width='400'>
+
+View full case study [here]()
+
+## Introduction
+In June 2020 - large scale supply changes were made at Data Mart, Danny’s latest venture and after running international operations for his online supermarket that specialises in fresh produce. All Data Mart products now use sustainable packaging methods in every single step from the farm all the way to the customer. Danny needs our help to quantify the impact of this change on the sales performance for Data Mart and it’s separate business areas.
+
+## Datasets
+<img src='https://8weeksqlchallenge.com/images/case-study-5-erd.png'>
+
+For this case study there is only a single table - `weekly_sales`, which:
+
+* has international operations using a multi-`region` strategy
+* has both, a retail and online `platform` in the form of a Shopify store front to serve their customers
+* Customer `segment` and `customer_type` data relates to personal age and demographics information that is shared with Data Mart
+* `transactions` is the count of unique purchases made through Data Mart and sales is the actual dollar amount of purchases
+* Each record in the dataset is related to a specific aggregated slice of the underlying sales data rolled up into a `week_date` value which represents the start of the sales week.
+
+View SQL schema file [here](./Schema.sql) 
+
+## Case Study Questions
+
+### 1. Data Cleansing Steps - [Solution](./1.%20Data%20Cleansing%20Steps.md)
+
+<p>In a single query, perform the following operations and generate a new table in the <code class="language-plaintext highlighter-rouge">data_mart</code> schema named <code class="language-plaintext highlighter-rouge">clean_weekly_sales</code>:</p>
+
+<ul>
+  <li>
+    <p>Convert the <code class="language-plaintext highlighter-rouge">week_date</code> to a <code class="language-plaintext highlighter-rouge">DATE</code> format</p>
+  </li>
+  <li>
+    <p>Add a <code class="language-plaintext highlighter-rouge">week_number</code> as the second column for each <code class="language-plaintext highlighter-rouge">week_date</code> value, for example any value from the 1st of January to 7th of January will be 1, 8th to 14th will be 2 etc</p>
+  </li>
+  <li>
+    <p>Add a <code class="language-plaintext highlighter-rouge">month_number</code> with the calendar month for each <code class="language-plaintext highlighter-rouge">week_date</code> value as the 3rd column</p>
+  </li>
+  <li>
+    <p>Add a <code class="language-plaintext highlighter-rouge">calendar_year</code> column as the 4th column containing either 2018, 2019 or 2020 values</p>
+  </li>
+  <li>
+    <p>Add a new column called <code class="language-plaintext highlighter-rouge">age_band</code> after the original <code class="language-plaintext highlighter-rouge">segment</code> column using the following mapping on the number inside the <code class="language-plaintext highlighter-rouge">segment</code> value</p>
+  </li>
+</ul>
+
+<table>
+  <thead>
+    <tr>
+      <th>segment</th>
+      <th>age_band</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Young Adults</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Middle Aged</td>
+    </tr>
+    <tr>
+      <td>3 or 4</td>
+      <td>Retirees</td>
+    </tr>
+  </tbody>
+</table>
+
+<ul>
+  <li>Add a new <code class="language-plaintext highlighter-rouge">demographic</code> column using the following mapping for the first letter in the <code class="language-plaintext highlighter-rouge">segment</code> values:</li>
+</ul>
+
+<table>
+  <tbody>
+    <tr>
+      <td>segment</td>
+      <td>demographic</td>
+    </tr>
+    <tr>
+      <td>C</td>
+      <td>Couples</td>
+    </tr>
+    <tr>
+      <td>F</td>
+      <td>Families</td>
+    </tr>
+  </tbody>
+</table>
+
+<ul>
+  <li>
+    <p>Ensure all <code class="language-plaintext highlighter-rouge">null</code> string values with an <code class="language-plaintext highlighter-rouge">"unknown"</code> string value in the original <code class="language-plaintext highlighter-rouge">segment</code> column as well as the new <code class="language-plaintext highlighter-rouge">age_band</code> and <code class="language-plaintext highlighter-rouge">demographic</code> columns</p>
+  </li>
+  <li>
+    <p>Generate a new <code class="language-plaintext highlighter-rouge">avg_transaction</code> column as the <code class="language-plaintext highlighter-rouge">sales</code> value divided by <code class="language-plaintext highlighter-rouge">transactions</code> rounded to 2 decimal places for each record</p>
+  </li>
+</ul>
+
+### 2. Data Exploration - [Solution](./2.%20Data%20Exploration.md)
+
+<ol>
+  <li>What day of the week is used for each <code class="language-plaintext highlighter-rouge">week_date</code> value?</li>
+  <li>What range of week numbers are missing from the dataset?</li>
+  <li>How many total transactions were there for each year in the dataset?</li>
+  <li>What is the total sales for each region for each month?</li>
+  <li>What is the total count of transactions for each platform</li>
+  <li>What is the percentage of sales for Retail vs Shopify for each month?</li>
+  <li>What is the percentage of sales by demographic for each year in the dataset?</li>
+  <li>Which <code class="language-plaintext highlighter-rouge">age_band</code> and <code class="language-plaintext highlighter-rouge">demographic</code> values contribute the most to Retail sales?</li>
+  <li>Can we use the <code class="language-plaintext highlighter-rouge">avg_transaction</code> column to find the average transaction size for each year for Retail vs Shopify? If not - how would you calculate it instead?</li>
+</ol>
+
+### 3. Before & After Analysis - [Solution](3.%20Before%20&%20After%20Analysis.md) 
+
+<p>This technique is usually used when we inspect an important event and want to inspect the impact before and after a certain point in time.</p>
+
+<p>Taking the <code class="language-plaintext highlighter-rouge">week_date</code> value of <code class="language-plaintext highlighter-rouge">2020-06-15</code> as the baseline week where the Data Mart sustainable packaging changes came into effect.</p>
+
+<p>We would include all <code class="language-plaintext highlighter-rouge">week_date</code> values for <code class="language-plaintext highlighter-rouge">2020-06-15</code> as the start of the period <strong>after</strong> the change and the previous <code class="language-plaintext highlighter-rouge">week_date</code> values would be <strong>before</strong></p>
+
+<p>Using this analysis approach - answer the following questions:</p>
+
+<ol>
+  <li>What is the total sales for the 4 weeks before and after <code class="language-plaintext highlighter-rouge">2020-06-15</code>? What is the growth or reduction rate in actual values and percentage of sales?</li>
+  <li>What about the entire 12 weeks before and after?</li>
+  <li>How do the sale metrics for these 2 periods before and after compare with the previous years in 2018 and 2019?</li>
+</ol>
+
+### 4. Bonus Question - [Solution](./4.%20Bonus%20Question.md)
+
+<p>Which areas of the business have the highest negative impact in sales metrics performance in 2020 for the 12 week before and after period?</p>
+
+<ul>
+  <li><code class="language-plaintext highlighter-rouge">region</code></li>
+  <li><code class="language-plaintext highlighter-rouge">platform</code></li>
+  <li><code class="language-plaintext highlighter-rouge">age_band</code></li>
+  <li><code class="language-plaintext highlighter-rouge">demographic</code></li>
+  <li><code class="language-plaintext highlighter-rouge">customer_type</code></li>
+</ul>
+
+<p>Do you have any further recommendations for Danny’s team at Data Mart or any interesting insights based off this analysis?</p>
+
+
+<br>
+
+View full SQL query file [here](./Query.sql).
